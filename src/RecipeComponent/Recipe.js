cdd56bb40933098af3b8c625c421fe60ee6920c7
ChahelPaatur <75865413+ChahelPaatur@users.noreply.github.com>
2024-10-20 14:51:37 -0700

    Update Recipe.js

--- src/RecipeComponent/Recipe.js
+++ src/RecipeComponent/Recipe.js
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import './Recipe.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faPepperHot } from "@fortawesome/free-solid-svg-icons";
+import '../../src/imgs/'
 const Recipe = () => {
     const location = useLocation()
     const {state} = location || {}; 
